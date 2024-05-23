7304b403feeb46349ad52d87b44932e50dea4825
mjayy77 <billymj77@gmail.com>
2024-05-24 02:12:11 +0700

    first_API

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *           version="1.0",
+ *           title="My Rest API",
+ *           description="Laravel project to practice developing Rest API with L5 Swagger OpenApi",
+ *           x={
+ *           "logo": {
+ *           "url": "https://miro.medium.com/v2/resize:fit:1200/1*J3G3akaMpUOLeqw0p0qthA.png"
+ *              }
+ *            },
+ *            @OA\Contact(
+ *                  name="Billy",
+ *                  email="billy.42223018@civitas.ukrida.ac.id"
+ *            ),
+ *  )
+ */
+abstract class Controller
+{
+    
+}
